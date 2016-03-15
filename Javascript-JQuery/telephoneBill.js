f559f8f0fd5bf366c51ebfb479f9cb98238ba7a9
Ricardo Osit <aizekrysler@gmail.com>
2016-03-15 08:40:31 -0700

    fix for 60mins
    fix for 60mins

--- Javascript-JQuery/telephoneBill.js
+++ Javascript-JQuery/telephoneBill.js
@@ -22,12 +22,11 @@ function getElapsedTime() {
 	if(endTime_hr == startTime_hr) { //if same hour
 		elapseTime = elapsedTime_min;
 	}else if(endTime_min == 00 && startTime_min == 00) { //if both has exact time
-		elapseTime = elapsedTime_hr
+		elapseTime = elapsedTime_hr*60;
 	}
 	else {
 		elapseTime = elapsedTime_min+60;
 	}
-	
 	//document.write(elapseTime);
 	return elapseTime;
 	
